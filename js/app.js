60d12e8085aae09cf7277a28a5c14f54dbe7d04f
Ihsara <longchau21@gmail.com>
2019-02-19 12:17:54 +0200

    Add all needed functionalities for submission

--- js/app.js
+++ js/app.js
@@ -13,7 +13,9 @@ const cards = ['fa-diamond', 'fa-diamond',
              'fa-bomb', 'fa-bomb'];
 const moveCounter = document.querySelector('.moves');
 const restart = document.querySelector('.restart');
-const refresh=document.querySelector('#modalBtn');
+const start3 = document.querySelector('#star3');
+const start2 = document.querySelector('#star2');
+
 let moves = 0;
 function generateCard(card){
     return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
@@ -48,6 +50,8 @@ function initGame(){
     moveCounter.innerHTML = moves;
 
     deck.innerHTML = cardHTML.join('');
+    setVisibleStar(star3);
+    setVisibleStar(star2);
     console.log("New Game loaded!")
 }
 
@@ -79,12 +83,49 @@ function addMoveCounter(){
     moveCounter.innerText = moves
 }
 
+function starRating(sec, moves){
+    if (sec < 50 && moves < 25){
+        return 3; 
+    }else if (sec <60 && moves<30){
+        return 2;
+    }else{
+        return 1; 
+    }
+}
+
+function setHiddenStar(star){
+    star.style.visibility = 'hidden';
+}
+
+function setVisibleStar(star){
+    star.style.visibility = 'visible';
+}
+
+function rating(sec, moves){
+    if (starRating(sec, moves) == 2){ 
+        setHiddenStar(star3); 
+    }else if (starRating(sec, moves) == 1){
+        setHiddenStar(star3);
+        setHiddenStar(star2); 
+    }
+}
+function ratingStarModal(star1, star2, sec, moves){
+    if (starRating(sec, moves) == 2){ 
+        setHiddenStar(star2); 
+    }else if (starRating(sec, moves) == 1){
+        setHiddenStar(star2);
+        setHiddenStar(star1); 
+    }
+}
 // Taken from https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
 let sec = 0;
 function pad ( val ) { return val > 9 ? val : "0" + val; }
-setInterval( function(){
-    document.getElementById("seconds").innerHTML=pad(++sec%60);
-    document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+setInterval( ()=>{
+    if (!gameDone && gameStart){
+        document.getElementById("seconds").innerHTML=pad(++sec%60);
+        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+    };
+    rating(sec, moves);
 }, 1000);
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -100,10 +141,13 @@ setInterval( function(){
 initGame(); 
 const allCards = document.querySelectorAll('.card');
 let openCards = [];
+let match = 0;
+let gameDone=false;
+let gameStart=false;
 
-
-allCards.forEach((card)=>{    
+allCards.forEach((card)=>{
     card.addEventListener('click', (e)=>{
+        gameStart=true; 
         openCards = addToOpencards(openCards, card); 
         if (openCards.length == 2){
             if (openCards[0].dataset.card == openCards[1].dataset.card){
@@ -111,12 +155,27 @@ allCards.forEach((card)=>{
                 addMatchToCard(openCards[0]);
                 addMatchToCard(openCards[1]);
                 openCards = [];
+                if (++match == 8){
+                    modalVictory.setContent(`<h1>Victory!</h1><p>Time: ${pad(parseInt(sec/60,10))}:${pad(sec%60)}</p><section class="score-panel">   <ul class="stars">
+                    <li><i id="star4" class="fa fa-star"></i></li>
+                    <li><i id="star5" class="fa fa-star"></i></li>
+                    <li><i id="star6" class="fa fa-star"></i></li>
+                    </ul></section>`);
+                    const star5 = document.querySelector('#star5');
+                    const star6 = document.querySelector('#star6');
+                    ratingStarModal(star6,star5,sec , moves); 
+                    // open modal
+                    modalVictory.open();
+                    sec=0;
+                    gameDone=true;
+                    gameStart=false;
+                }
             }else {
                 setTimeout(() => {
                     openCards = removeOpenShowFromCard(openCards);
                 }, 1000);
             }
-            addMoveCounter(); 
+            addMoveCounter();             
         }
     });
 });
@@ -124,11 +183,15 @@ allCards.forEach((card)=>{
 restart.addEventListener('click', ()=>{
     initGame(); 
     const allCards = document.querySelectorAll('.card');
-    let openCards = [];
-
+    openCards = [];
+    match=0;
+    gameDone=false;
+    gameStart=false;
+    sec=0;
 
     allCards.forEach((card)=>{    
         card.addEventListener('click', (e)=>{
+            gameStart=true; 
             openCards = addToOpencards(openCards, card); 
             if (openCards.length == 2){
                 if (openCards[0].dataset.card == openCards[1].dataset.card){
@@ -136,6 +199,21 @@ restart.addEventListener('click', ()=>{
                     addMatchToCard(openCards[0]);
                     addMatchToCard(openCards[1]);
                     openCards = [];
+                    if (++match == 8){
+                        modalVictory.setContent(`<h1>Victory!</h1><p>Time: ${pad(parseInt(sec/60,10))}:${pad(sec%60)}</p><section class="score-panel">   <ul class="stars">
+                        <li><i id="star4" class="fa fa-star"></i></li>
+                        <li><i id="star5" class="fa fa-star"></i></li>
+                        <li><i id="star6" class="fa fa-star"></i></li>
+                        </ul></section>`);
+                        const star5 = document.querySelector('#star5');
+                        const star6 = document.querySelector('#star6');
+                        ratingStarModal(star6,star5, sec, moves);                         
+                        // open modal
+                        modalVictory.open();
+                        sec=0;
+                        gameDone=true;
+                        gameStart=false;
+                    }                    
                 }else {
                     setTimeout(() => {
                         openCards = removeOpenShowFromCard(openCards);
@@ -147,8 +225,8 @@ restart.addEventListener('click', ()=>{
     });    
 });
 
-/* // instanciate new modal
-var modal = new tingle.modal({
+// instanciate new modal
+var modalVictory = new tingle.modal({
     footer: true,
     stickyFooter: false,
     closeMethods: ['overlay', 'button', 'escape'],
@@ -164,24 +242,64 @@ var modal = new tingle.modal({
         // here's goes some logic
         // e.g. save content before closing the modal
         return true; // close the modal
-        return false; // nothing happens
     }
 });
 
-// set content
-modal.setContent('<h1>Victory!</h1>');
-
 // add a button
-modal.addFooterBtn('Okay', 'tingle-btn tingle-btn--primary', ()=> {
+modalVictory.addFooterBtn('Okay', 'tingle-btn tingle-btn--primary', ()=> {
     // here goes some logic
-    modal.close();
+    modalVictory.close();
 });
 
 // add another button
-modal.addFooterBtn('Replay!', 'tingle-btn tingle-btn--danger',() =>{
+modalVictory.addFooterBtn('Replay!', 'tingle-btn tingle-btn--danger',() =>{
     // here goes some logic
-    modal.close();
+    modalVictory.close();
 });
 
-// open modal
-modal.open(); */
+const replay = document.querySelector('.tingle-btn--danger');
+
+replay.addEventListener('click', ()=>{
+    initGame(); 
+    const allCards = document.querySelectorAll('.card');
+    openCards = [];
+    match=0;
+    gameDone=false;
+    gameStart=false;
+    sec=0;
+
+    allCards.forEach((card)=>{    
+        card.addEventListener('click', (e)=>{
+            gameStart=true; 
+            openCards = addToOpencards(openCards, card); 
+            if (openCards.length == 2){
+                if (openCards[0].dataset.card == openCards[1].dataset.card){
+                    console.log('Match!');
+                    addMatchToCard(openCards[0]);
+                    addMatchToCard(openCards[1]);
+                    openCards = [];
+                    if (++match == 8){
+                        modalVictory.setContent(`<h1>Victory!</h1><p>Time: ${pad(parseInt(sec/60,10))}:${pad(sec%60)}</p><section class="score-panel">   <ul class="stars">
+                        <li><i id="star4" class="fa fa-star"></i></li>
+                        <li><i id="star5" class="fa fa-star"></i></li>
+                        <li><i id="star6" class="fa fa-star"></i></li>
+                        </ul></section>`);
+                        const star5 = document.querySelector('#star5');
+                        const star6 = document.querySelector('#star6');
+                        ratingStarModal(star6,star5, sec, moves);                         
+                        // open modal
+                        modalVictory.open();
+                        sec=0;
+                        gameDone=true;
+                        gameStart=false;
+                    }                    
+                }else {
+                    setTimeout(() => {
+                        openCards = removeOpenShowFromCard(openCards);
+                    }, 1000);
+                }
+                addMoveCounter(); 
+            }
+        });
+    });    
+});
